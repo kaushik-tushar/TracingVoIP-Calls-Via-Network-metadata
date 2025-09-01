d7174d55dd3ac029e7ce05318a9b15431d56522d
TUSHAR KAUSHIK <163981654+kaushik-tushar@users.noreply.github.com>
2025-09-01 23:47:03 +0530

    Delete backend directory

--- backend/capture/__init__.py
+++
@@ -1,6 +0,0 @@
-# backend/capture/__init__.py
-
-from .capture_packets import process_packet, get_stats, start_capture
-from .pcap_reader import read_pcap
-
-__all__ = ["process_packet", "get_stats", "start_capture", "read_pcap"]
